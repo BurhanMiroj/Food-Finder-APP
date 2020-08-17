73116929df13c247dc56bc3f05bf35016794e2f6
Burhanudin M <mirojburhanudin2@gmail.com>
2020-08-17 09:59:36 +0700

    Ubah Placeholder

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const hamburgerMenu = document.querySelector(".menu-hamburger");
-const menuList = document.querySelector(".list-menu");
+const menuList = document.querySelector(".menuList");
 
 hamburgerMenu.addEventListener("click", function () {
-    
+   alert("bvcidvkbfsv")
+    // menuList.classList.remove("hidden")
 });
