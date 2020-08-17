b49dc2b521d4fe5abed59fc73e30d9fb9c43ea67
Burhanudin M <mirojburhanudin2@gmail.com>
2020-08-17 09:31:24 +0700

    revisi

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const hamburgerMenu = document.querySelector(".menu-hamburger");
+const menuList = document.querySelector(".list-menu");
+
+hamburgerMenu.addEventListener("click", function () {
+    // menuList.classList.toggle('hidden');
+    alert("ajdv")
+});
